e45f8389a3219ff20bc1d18ca20f92b7ef8ca050
leo <leo@autonomous.ai>
2026-03-26 14:51:07 +0700

    - fix issue

--- lumi/cmd/bootstrap/main.go
+++ lumi/cmd/bootstrap/main.go
@@ -3,7 +3,9 @@ package main
 import (
 	"flag"
 	"fmt"
+	"io"
 	"log"
+	"os"
 
 	"go-lamp.autonomous.ai/bootstrap"
 	"go-lamp.autonomous.ai/bootstrap/config"
@@ -17,6 +19,13 @@ func main() {
 	if showVersion {
 		fmt.Println(config.BootstrapVersion)
 		return
+	}
+
+	// Write logs to both stdout and file for easier debugging
+	logFile, err := os.OpenFile("/var/log/bootstrap.log", os.O_CREATE|os.O_WRONLY|os.O_APPEND, 0644)
+	if err == nil {
+		log.SetOutput(io.MultiWriter(os.Stdout, logFile))
+		defer logFile.Close()
 	}
 
 	b, err := bootstrap.ProvideServer()
