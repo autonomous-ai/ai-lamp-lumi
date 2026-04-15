a00f6743952d3e5a288f511f4560588d2789f514
leo <genesis0000@bvm.network>
2026-04-15 15:51:20 +0700

    - fix bug

--- lumi/web/vite.config.ts
+++ lumi/web/vite.config.ts
@@ -1,6 +1,6 @@
 import fs from "fs";
 import path from "path";
-import { defineConfig } from "vite";
+import { defineConfig, loadEnv } from "vite";
 import react from "@vitejs/plugin-react";
 import tailwindcss from "@tailwindcss/vite";
 
