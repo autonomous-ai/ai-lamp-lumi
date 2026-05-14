a20b8d04b6024b53fbbdc7b5e75e9cd0c4c208c9
trnkhanh2908 <khanh29082003@gmail.com>
2026-05-14 17:49:50 +0700

    [dlbackend] Add base classes

---
+++ dlbackend/src/core/perception/base/__init__.py
@@ -0,0 +1,5 @@
+from .perception import PerceptionBase
+from .predictor import PredictorBase
+from .session import PerceptionSessionBase
+
+__all__ = ["PerceptionBase", "PredictorBase", "PerceptionSessionBase"]
---
+++ dlbackend/src/core/perception/base/session.py
@@ -0,0 +1,63 @@
+import logging
+import secrets
+from abc import ABC, abstractmethod
+from dataclasses import asdict
+from typing import Generic, TypeVar
+
+from core.types import Omit
+
+INPUT_T = TypeVar("INPUT_T")
+OUTPUT_T = TypeVar("OUTPUT_T")
+CONFIG_T = TypeVar("CONFIG_T")
+CONFIG_UPDATE_T = TypeVar("CONFIG_UPDATE_T")
+
+
+class PerceptionSessionBase(Generic[INPUT_T, OUTPUT_T, CONFIG_T, CONFIG_UPDATE_T], ABC):
+    def __init__(self, config: CONFIG_T) -> None:
+        self._logger: logging.Logger = logging.getLogger(
+            f"{self.__class__.__module__}.{self.__class__.__name__}"
+        )
+        self._logger.setLevel(logging.DEBUG)
+
+        self._config: CONFIG_T = config
+        self._last_update_ts: float = -1
+        self._last_prediction: OUTPUT_T | None = None
+
+        self._session_id: str = secrets.token_hex(8)
+
+    @property
+    def session_id(self) -> str:
+        return self._session_id
+
+    @abstractmethod
+    async def start(self) -> None:
+        pass
+
+    @abstractmethod
+    async def stop(self) -> None:
+        pass
+
+    @abstractmethod
+    def is_ready(self) -> bool:
+        pass
+
+    def _post_config_update(self):
+        pass
+
+    def set_config(self, config: CONFIG_T) -> None:
+        self._config = config
+        self._post_config_update()
+
+    def update_config(self, config: CONFIG_UPDATE_T) -> None:
+        for k, v in asdict(config).items():
+            if not isinstance(v, Omit):
+                self._config.__setattr__(k, v)
+        self._post_config_update()
+
+    @abstractmethod
+    async def update(self, input: INPUT_T) -> OUTPUT_T | None:
+        pass
+
+    @property
+    def last_prediction(self):
+        return self._last_prediction
