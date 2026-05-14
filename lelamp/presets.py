a49ff92997d7ced64801bc12ae31b9972d9dc408
leo <leo@autonomous.ai>
2026-05-14 11:21:42 +0700

    - aim down

--- lelamp/presets.py
+++ lelamp/presets.py
@@ -172,7 +172,7 @@ AIM_PRESETS = {
     AIM_WALL:   {"base_yaw.pos": 3.0,   "base_pitch.pos": 5.0,   "elbow_pitch.pos": -20.0, "wrist_roll.pos": 0.0, "wrist_pitch.pos": -60.0},
     AIM_LEFT:   {"base_yaw.pos": -90.0, "base_pitch.pos": -30.0, "elbow_pitch.pos": 57.0, "wrist_roll.pos": 0.0, "wrist_pitch.pos": 18.0},
     AIM_RIGHT:  {"base_yaw.pos": 90.0,  "base_pitch.pos": -30.0, "elbow_pitch.pos": 57.0, "wrist_roll.pos": 0.0, "wrist_pitch.pos": 18.0},
-    AIM_UP:     {"base_yaw.pos": 3.0,   "base_pitch.pos": 10.0,  "elbow_pitch.pos": 5.0,  "wrist_roll.pos": 0.0, "wrist_pitch.pos": 25.0},
-    AIM_DOWN:   {"base_yaw.pos": 3.0,   "base_pitch.pos": -50.0, "elbow_pitch.pos": 60.0, "wrist_roll.pos": 0.0, "wrist_pitch.pos": -25.0},
+    AIM_UP:     {"base_yaw.pos": 3.0,   "base_pitch.pos": 10.0,  "elbow_pitch.pos": -15.0,  "wrist_roll.pos": 0.0, "wrist_pitch.pos": 25.0},
+    AIM_DOWN:   {"base_yaw.pos": 3.0,   "base_pitch.pos": -90.0, "elbow_pitch.pos": 90.0, "wrist_roll.pos": 0.0, "wrist_pitch.pos": -90.0},
     AIM_USER:   {"base_yaw.pos": 0.0,   "base_pitch.pos": 0.0,   "elbow_pitch.pos": 0.0,  "wrist_roll.pos": 0.0, "wrist_pitch.pos": -45.0},
 }
